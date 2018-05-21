da42daa0fc5daed49acc87ee53b2c00ffe16ed43
bbmadriaga <bbmadriaga@yahoo.com>
2018-05-21 09:41:40 +0800

    update helper file

--- conf/helper.php
+++ conf/helper.php
@@ -3,8 +3,8 @@
 require_once(dirname(dirname(__FILE__)) . '/inc/class/jdb.php');
 
 // Database
-define ( 'DB_HOST', '192.168.3.201' );
-define ( 'DB_USER', 'apache1' );
-define ( 'DB_PASSWORD', 'QdSp4&NWMZ6tD/&j' );
-define ( 'DB_DB', 'csc_jportaldb' );
+define ( 'DB_HOST', ' ' );
+define ( 'DB_USER', ' ' );
+define ( 'DB_PASSWORD', ' ' );
+define ( 'DB_DB', ' ' );
 ?>
